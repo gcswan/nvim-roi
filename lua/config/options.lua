536db521ac244b11a6028e0d340b33ccc4c032ea
Greg Swan <greg.c.swan@gmail.com>
2025-01-12 16:04:07 -0500

    changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,5 +4,11 @@
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 -- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
 vim.g.lazyvim_blink_main = false
+
+--- FORMATTING ---
+vim.g.lazyvim_eslint_auto_format = true
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,57 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "sainnhe/sonokai" },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        cmp = true,
+        fzf = true,
+        gitsigns = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotree = true,
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+}
