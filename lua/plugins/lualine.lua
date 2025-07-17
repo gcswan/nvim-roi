ba4e01a57e7dff29f07389413eedaf953979681c
Greg Swan <greg.c.swan@gmail.com>
2025-07-17 00:19:08 -0400

    tokyonight lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -22,7 +22,7 @@ return {
 
     local opts = {
       options = {
-        theme = "gruvbox-material",
+        theme = "tokyonight",
         globalstatus = vim.o.laststatus == 3,
         disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
       },
