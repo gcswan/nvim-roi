ee0b8fcdfebe5804aeee3ffc68defdfd6ac344b5
Greg Swan <greg.c.swan@gmail.com>
2025-07-17 00:16:01 -0400

    added tokyonight colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,45 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("tokyonight").setup({
+        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "transparent", -- style for sidebars, see below
+          floats = "transparent", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+        on_colors = function(colors)
+          colors.hint = colors.orange
+          colors.error = "#ff0000"
+        end,
+        on_highlights = function(highlights, colors)
+          -- Make backgrounds transparent
+          highlights.Normal = { bg = "none" }
+          highlights.NormalFloat = { bg = "none" }
+          highlights.NormalNC = { bg = "none" }
+          highlights.SignColumn = { bg = "none" }
+        end,
+      })
+    end,
+  },
+  {
     "ellisonleao/gruvbox.nvim",
     config = function()
       require("gruvbox").setup({
@@ -382,12 +422,13 @@ return {
       -- colorscheme = "gruvbox",
       -- colorscheme = "dracula",
       -- colorscheme = "kanagawa",
-      colorscheme = "sonokai",
+      -- colorscheme = "sonokai",
       -- colorscheme = "cyberdream",
       -- colorscheme = "catppuccin",
       -- colorscheme = "gruvbox-material",
       -- colorscheme = "nordic",
       -- colorscheme = "fluoromachine",
+      colorscheme = "tokyonight",
     },
   },
 }
